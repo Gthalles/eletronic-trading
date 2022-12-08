ea399b5edbffb17a0762446a09d09ca67baa4158
BeatrizBanaki <beatrizbanaki@gmail.com>
2022-12-07 23:04:10 -0300

    cria client, service, controller e route para a listagem de usuario

---
+++ eletronic-trading-backend/src/controller/UsersListController.ts
@@ -0,0 +1,24 @@
+import { UsersListService } from "../service";
+import { Request, Response } from "express";
+
+class UsersListController {
+    private readonly usersListService = new UsersListService();
+
+    public async handle(req: Request, res: Response) {
+        try {
+            await this.usersListService.execute();
+
+            return res.status(200).send("User has ben registered.");
+        } catch(error: any) {
+            const [statusCode, message ] = error.message.split(":");
+
+            if(statusCode > 99 && statusCode < 600) {
+                return res.status(statusCode).send(message);
+            }
+
+            return res.status(500).send("Unexpected error in user list controller.");
+        }
+    }
+}
+
+export { UsersListController };
--- eletronic-trading-backend/src/route/user.ts
+++ eletronic-trading-backend/src/route/user.ts
@@ -1,10 +1,15 @@
 import { UserRegistrationController } from "../controller";
 import { Router } from "express";
+import { UsersListController } from "../controller/UsersListController";
 
 const userRegistrationController = new UserRegistrationController();
+
+const usersListController = new UsersListController();
 
 const router = Router();
 
 router.post("/user/register", userRegistrationController.handle.bind(userRegistrationController));
+
+router.get("/users/list", usersListController.handle.bind(UsersListController));
 
 export default router;
